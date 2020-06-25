f2ed15fb4d7916f0d7130c7de9d4cd1a927b3ca5
Serghei <serghei.cunev@gmail.com>
2020-06-26 01:16:34 +0300

    Update mare#2
    Am făcut widget de adăugare a comentariilor(jquery,javascript,html), am modificat pagina home, am modificat footer-urile la toate păginile, un fișier nou javascript,am modificat imaginile din background, am schimbat un pic stilurile din CSS

---
+++ js/serghei.js
@@ -0,0 +1,43 @@
+var commentform = document.getElementById('commentForm');
+
+function showDiv() {
+  commentform.style.display = "block";
+}
+
+function closeDiv() {
+  commentform.style.display = "none";
+}
+
+function showOrHide() {
+  if (commentform.style.display === "block") {
+    closeDiv()
+  } else {
+    showDiv()
+  }
+}
+comentarii = [
+    {name:'Ștefan-Robert Netu',comment:'În calitate de profesor de UI/UX la webdemy am ajutat peste 1000 de studenți.'},
+    {name:'Serghei Cunev',comment:'Serviciile noastre au ajutat foarte mulți tineri să-și găsească un path nou în carieră'},
+    {name:'Alexandru Zaporojan',comment:'Sunt entuziasmat că pot aduce studenții mai aproape de piața de muncă'},
+    {name:'Ion Dodon',comment:'webdemy m-a ajutat să înțeleg ce nu am înțeles la cursurile de la facultate'},
+    {name:'Igor Chicu',comment:'Simplu,scurt,clar,cuprinzător.Vă Mulțumesc.'}
+]
+comentarii.forEach(e => {
+    addComment(e.name,e.comment)
+})
+
+
+
+function addComment(name,comentariu) { 
+name == undefined ? name = document.querySelector("#commentForm > form > div.form-field > textarea").value :0
+comentariu==undefined ? comentariu=document.querySelector("#commentForm > form > div.form-field > textarea").value:0
+
+ $('.owl-carousel').owlCarousel('add',  `
+ <div class="single-slide">
+ <div class="content">
+     <p>${comentariu}</p>
+     <h4>${name}</h4>
+ </div>
+</div>
+ `).owlCarousel('update');
+}
